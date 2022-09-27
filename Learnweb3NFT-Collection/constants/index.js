75bd1b2616ea407271ed63df0ac2c46c120bf119
ronkips <kipshillash@gmail.com>
2022-09-27 20:10:07 +0300

    restructure the metadata

--- Learnweb3NFT-Collection/constants/index.js
+++ Learnweb3NFT-Collection/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xefDEeC983C3C8bABBeAF64aAc3b1815E5DF431c9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://vercel.com/ronkips/learnweb3-collection/api/";
+const METADATA_URL = "https://learnweb3-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
